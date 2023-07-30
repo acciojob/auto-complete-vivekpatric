0495a2bcc488308369a620be9142cd1786e28a2b
vivekpatric <patricleo223@gmail.com>
2023-07-30 21:27:19 +0530

    result

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+
 const App = () => {
+  const [search, setSearch] = useState("");
+  const [filteredItem, setFilteredItem] = useState([...fruits]);
+
+  useEffect(() => {
+    // if (!search) setFilteredItem([...fruits]);
+    // else 
+    let filteredFruits = fruits.filter((fruit) => fruit.includes(search));
+    setFilteredItem(filteredFruits);
+  }, [search])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onInput={(e) => setSearch(e.target.value)}></input>
+      {/* Do not remove the main div */}
+      <ul>
+        {
+          filteredItem.map((filteredFruit) => (
+            <li>{filteredFruit}</li>
+          ))
+        }
+      </ul>
+
     </div>
   )
 }
 
-export default App
+// const App = () => {
+//   return(
+//     <div>
+//       <h1>Hello</h1>
+//     </div>
+//   )
+// }
+
+export default App;
